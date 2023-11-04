54b6eb6961e169f626bc7b6c9f74ad409b839051
thorbie <jonahemmanuel54@gmail.com>
2023-11-04 11:52:34 +0100

    hbnb

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    const amenityIds = {}; // Store Amenity IDs in an object
+
+    $('input[type="checkbox"]').change(function () {
+        const amenityId = $(this).attr("data-id");
+        const amenityName = $(this).attr("data-name");
+
+        if (this.checked) {
+            amenityIds[amenityId] = amenityName;
+        } else {
+            delete amenityIds[amenityId];
+        }
+
+        // Update the h4 tag with the list of Amenities checked
+        const amenitiesList = Object.values(amenityIds).join(', ');
+        const $h4 = $('.amenities h4');
+        $h4.text(amenitiesList);
+        $h4.addClass('ellipsis-text');
+        $h4.attr('title', amenitiesList);
+    });
+});
