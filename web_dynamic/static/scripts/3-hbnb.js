43344bb5cbdf1e32a84db5feccb3254c5478c1c4
thorbie <jonahemmanuel54@gmail.com>
2023-11-05 14:53:34 +0100

    updates

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -28,21 +28,23 @@ $.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
 });
 
 $.post({
-	url: 'http://0.0.0.0:5001/api/v1/places_search/',
-	contentType: 'application/json',
-	data: JSON.stringify({}),
-	success: (data) => {
-		$(data).each() {
-			$('.places').append(
-				$('<article>').append(
-					$('<div class="price_by_night">').text('$' + $(this).attr('price_by_night')),
-					$('<h2></h2>').text($(this).attr('name')),
-					$('<div class="informations"></div>').append(
-						$('<div class="max_guest"></div>').text($(this).attr('max_guest') + ' Guests'),
-						$('<div class="number_rooms"></div>').text($(this).attr('number_rooms') + ' Rooms'),
-						$('<div class="number_bathrooms">').text($(this).attr('number_bathrooms') + ' Bathrooms'),
-						$('<div class="description"></div>').text($(this).attr('description')))
-				))
-		})}
-	})
-}
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+        $(data).each(function () {
+            $('.places').append(
+                $('<article>').append(
+                    $('<div class="price_by_night">').text('$' + $(this).attr('price_by_night')),
+                    $('<h2></h2>').text($(this).attr('name')),
+                    $('<div class="informations"></div>').append(
+                        $('<div class="max_guest"></div>').text($(this).attr('max_guest') + ' Guests'),
+                        $('<div class="number_rooms"></div>').text($(this).attr('number_rooms') + ' Rooms'),
+                        $('<div class="number_bathrooms">').text($(this).attr('number_bathrooms') + ' Bathrooms'),
+                        $('<div class="description"></div>').text($(this).attr('description'))
+                    )
+                )
+            );
+        });
+    }
+});
